677a556dc3d2ff79c7a64c73dfda8f8783468d50
DESKTOP-1CSI9JF\Alumno <dragontero2@gmail.com>
2024-10-29 11:06:12 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from "./saludo/saludo.component";
 import { TarjetaProductoComponent } from "./tarjeta-producto/tarjeta-producto.component";
 import { UsuarioComponent } from "./usuario/usuario.component";
+import { TablaComponent } from "./tabla/tabla.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SaludoComponent, TarjetaProductoComponent, UsuarioComponent],
+  imports: [RouterOutlet, SaludoComponent, TarjetaProductoComponent, UsuarioComponent, TablaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
